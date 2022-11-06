1ec51b76e21de9f0f189b38e6e5a2a327ce0d245
Maria T. Andersen <mteilmann93@gmail.com>
2022-11-06 17:37:48 +0100

    Require the dotenv library and call its config method immediately to load all environment variables from the .env file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 // Connect to database
-mongoose.connect("mongodb://localhost/members", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database..."));
