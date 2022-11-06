78403c13e2b11129d5e886ab183bc26b30d8e91c
Maria T. Andersen <mteilmann93@gmail.com>
2022-11-06 17:31:46 +0100

    Connect to database with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+// Connect to database
+mongoose.connect("mongodb://localhost/members", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database..."));
 
 app.listen(port, () => console.log("Server started..."));
