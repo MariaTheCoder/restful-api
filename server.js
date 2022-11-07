daa6e4754de7cb8c6a5617a26eb8427f7d627fa8
Maria T. Andersen <mteilmann93@gmail.com>
2022-11-07 22:01:32 +0100

    Set up server to accept json

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database..."));
 
+app.use(express.json());
+
 app.listen(port, () => console.log("Server started..."));
