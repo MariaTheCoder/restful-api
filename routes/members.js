0c77ff479d52a67434cbc1e852aa1610dfdea00d
Maria T. Andersen <mteilmann93@gmail.com>
2022-11-08 19:15:02 +0100

    The rest of the routes take id as a parameter and we need to run the same function to find the member in our stored data for all of these routes. Instead of repeating the same function, we use a middleware function to find the member. Remember to call next at the end of the middleware function, otherwise no data will be sent back to the user.

--- routes/members.js
+++ routes/members.js
@@ -13,8 +13,8 @@ router.get("/", async (req, res) => {
 });
 
 // Get one member
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getMember, (req, res) => {
+  res.send(res.member.name);
 });
 
 // Create one member
@@ -41,5 +41,20 @@ router.patch("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   //
 });
+
+async function getMember(req, res, next) {
+  let member;
+
+  try {
+    member = await Member.findById(req.params.id);
+    if (member == null) {
+      return res.status(404).json({ message: "Cannot find member" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.member = member;
+  next();
+}
 
 module.exports = router;
