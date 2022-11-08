f54e4ca9c9f7a070fed29089e4b0ffa7b41d7c46
Maria T. Andersen <mteilmann93@gmail.com>
2022-11-08 17:53:13 +0100

    Create routes and test with if it works. Send http requests via the route.rest file and see if response is as expected.

--- routes/members.js
+++ routes/members.js
@@ -1,4 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+// Get all members
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Get one member
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// Create one member
+router.post("/", (req, res) => {
+  //
+});
+
+// Update one member
+router.patch("/:id", (req, res) => {
+  //
+});
+
+// Delete one member
+router.delete("/:id", (req, res) => {
+  //
+});
+
 module.exports = router;
